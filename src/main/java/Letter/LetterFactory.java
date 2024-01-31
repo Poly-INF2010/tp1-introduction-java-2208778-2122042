9abc1683a41c05e6e5decabd7112c6e7f9bde501
Benjamin Perrier <benjamin.perrier@polymtl.ca>
2024-01-31 12:36:28 -0500

    Finished the work, all tests passed

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -2,6 +2,7 @@ package Letter;
 
 import Point.Point2d;
 import Shape.*;
+import org.w3c.dom.css.Rect;
 
 public final class LetterFactory {
     final static Double maxHeight = 150.0;
@@ -17,7 +18,22 @@ public final class LetterFactory {
      * @return BaseShape containing the letter A
      */
     public static BaseShape create_A()  {
-        return null;
+        Rectangle leftBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.rotate(leftBar.getCoords(), 0.3);
+        leftBar.translate(leftBar.getCoords(), new Point2d(-halfMaxWidth + halfStripeThickness, 0.0));
+
+        Rectangle rightBar = new Rectangle(stripeThickness, maxHeight);
+        rightBar.rotate(rightBar.getCoords(),-0.3);
+        rightBar.translate(rightBar.getCoords(), new Point2d(halfMaxWidth - halfStripeThickness, 0.0));
+
+        Rectangle midBar = new Rectangle(halfMaxWidth, stripeThickness);
+        midBar.translate(midBar.getCoords(), new Point2d(0.0,halfStripeThickness));
+
+        BaseShape letterA = new BaseShape();
+        letterA.add(leftBar);
+        letterA.add(rightBar);
+        letterA.add(midBar);
+        return letterA;
     }
 
     /** TODO
@@ -25,7 +41,27 @@ public final class LetterFactory {
      * @return BaseShape containing the letter B
      */
     public static BaseShape create_B() {
-        return null;
+        Rectangle leftBar = new Rectangle(stripeThickness, maxHeight);
+
+        Circle topEllipse = new Circle(halfMaxHeight);
+        topEllipse.translate(topEllipse.getCoords(), new Point2d(halfMaxWidth, (-halfMaxHeight+2*stripeThickness)));
+
+        Circle bottomEllipse = new Circle(halfMaxHeight);
+        bottomEllipse.translate(bottomEllipse.getCoords(), new Point2d(halfMaxWidth, halfMaxHeight - 2*stripeThickness));
+
+        Circle topRemoveEllipse = new Circle(halfMaxHeight - stripeThickness);
+        topRemoveEllipse.translate(topRemoveEllipse.getCoords(), new Point2d(halfMaxWidth, (-halfMaxHeight+2*stripeThickness)));
+
+        Circle bottomRemoveEllipse = new Circle(halfMaxHeight - stripeThickness);
+        bottomRemoveEllipse.translate(bottomRemoveEllipse.getCoords(), new Point2d(halfMaxWidth, halfMaxHeight-2*stripeThickness));
+
+        BaseShape letterB = new BaseShape();
+        letterB.add(leftBar);
+        letterB.add(topEllipse);
+        letterB.add(bottomEllipse);
+        letterB.remove(topRemoveEllipse);
+        letterB.remove(bottomRemoveEllipse);
+        return letterB;
     }
 
     /** TODO
@@ -33,7 +69,18 @@ public final class LetterFactory {
      * @return BaseShape containing the letter C
      */
     public static BaseShape create_C() {
-        return null;
+        Ellipse mainEllipse = new Ellipse(maxWidth, maxHeight);
+
+        Ellipse removeEllipse = new Ellipse(maxWidth - stripeThickness, maxHeight- stripeThickness);
+
+        Rectangle removeRectangle = new Rectangle(halfMaxWidth, halfMaxHeight);
+        removeRectangle.translate(removeRectangle.getCoords(), new Point2d(halfMaxWidth, 0.0));
+
+        BaseShape letterC = new BaseShape();
+        letterC.add(mainEllipse);
+        letterC.remove(removeEllipse);
+        letterC.remove(removeRectangle);
+        return letterC;
     }
 
     /** TODO
@@ -41,7 +88,23 @@ public final class LetterFactory {
      * @return BaseShape containing the letter E
      */
     public static BaseShape create_E() {
-        return null;
+        Rectangle leftBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.translate(leftBar.getCoords(), new Point2d(-halfMaxWidth, 0.0));
+
+        Rectangle topBar = new Rectangle(maxWidth, stripeThickness);
+        topBar.translate(topBar.getCoords(), new Point2d(0.0, -halfMaxHeight + halfStripeThickness));
+
+        Rectangle midBar = new Rectangle(maxWidth, stripeThickness);
+
+        Rectangle bottomBar = new Rectangle(maxWidth, stripeThickness);
+        bottomBar.translate(bottomBar.getCoords(), new Point2d(0.0, halfMaxHeight - halfStripeThickness));
+
+        BaseShape letterE = new BaseShape();
+        letterE.add(topBar);
+        letterE.add(leftBar);
+        letterE.add(midBar);
+        letterE.add(bottomBar);
+        return letterE;
     }
 
     /** TODO
@@ -49,7 +112,19 @@ public final class LetterFactory {
      * @return BaseShape containing the letter H
      */
     public static BaseShape create_H() {
-        return null;
+        Rectangle leftBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.translate(leftBar.getCoords(), new Point2d(-halfMaxWidth, 0.0));
+
+        Rectangle midBar = new Rectangle(maxWidth, stripeThickness);
+
+        Rectangle rightBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.translate(rightBar.getCoords(), new Point2d(halfMaxWidth, 0.0));
+
+        BaseShape letterH = new BaseShape();
+        letterH.add(leftBar);
+        letterH.add(midBar);
+        letterH.add(rightBar);
+        return letterH;
     }
 
     /** TODO
@@ -57,7 +132,20 @@ public final class LetterFactory {
      * @return BaseShape containing the letter N
      */
     public static BaseShape create_N() {
-        return null;
+        Rectangle leftBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.translate(leftBar.getCoords(), new Point2d(-halfMaxWidth, 0.0));
+
+        Rectangle midBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.rotate(midBar.getCoords(), -0.3);
+
+        Rectangle rightBar = new Rectangle(stripeThickness, maxHeight);
+        leftBar.translate(rightBar.getCoords(), new Point2d(halfMaxWidth, 0.0));
+
+        BaseShape letterN = new BaseShape();
+        letterN.add(leftBar);
+        letterN.add(midBar);
+        letterN.add(rightBar);
+        return letterN;
     }
 
     /** TODO
@@ -65,7 +153,13 @@ public final class LetterFactory {
      * @return BaseShape containing the letter O
      */
     public static BaseShape create_O() {
-        return null;
-    }
+        Ellipse mainEllipse = new Ellipse(maxWidth, maxHeight);
 
+        Ellipse removeEllipse = new Ellipse(maxWidth - stripeThickness, maxHeight- stripeThickness);
+
+        BaseShape letterO = new BaseShape();
+        letterO.add(mainEllipse);
+        letterO.remove(removeEllipse);
+        return letterO;
+    }
 }
